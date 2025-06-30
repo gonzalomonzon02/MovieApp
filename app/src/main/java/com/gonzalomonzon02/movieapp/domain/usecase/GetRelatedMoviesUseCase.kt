7d7ea68c9a2a281cf5ecc8fe5b2b356287399ce5
Gonzalo Monzon <gonzalomonzon02@gmail.com>
2025-06-30 14:39:29 -0300

    Test fixed

--- app/src/main/java/com/gonzalomonzon02/movieapp/domain/usecase/GetRelatedMoviesUseCase.kt
+++ app/src/main/java/com/gonzalomonzon02/movieapp/domain/usecase/GetRelatedMoviesUseCase.kt
@@ -5,6 +5,6 @@ import com.gonzalomonzon02.movieapp.domain.repository.MovieRepository
 
 class GetRelatedMoviesUseCase(private val movieRepository: MovieRepository) {
     suspend operator fun invoke(movieId: Int): List<Movie> {
-        return emptyList()// movieRepository.getRelatedMovies(movieId)
+        return movieRepository.getRelatedMovies(movieId)
     }
 } 
